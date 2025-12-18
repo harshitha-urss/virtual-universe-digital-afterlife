d10affd026ca2b555baee857c6c587d402538e72
Harshitha Urs <harshithaveerabhadraiah2642004@gmail.com>
2025-12-18 17:07:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-window.addEventListener("scroll", () => {
-    document.querySelectorAll("section").forEach(section => {
-        const position = section.getBoundingClientRect().top;
-        const trigger = window.innerHeight / 1.2;
 
-        if (position < trigger) {
-            section.style.opacity = "1";
-            section.style.transform = "translateY(0)";
-        }
-    });
-});
